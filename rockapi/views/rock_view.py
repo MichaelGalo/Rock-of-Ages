968b7b240ed911947885c8d642ba4c49f77829b8
Michael Galo <michael@michaelthinks.co>
2024-09-11 11:56:51 -0500

    Project scope acheived

--- rockapi/views/rock_view.py
+++ rockapi/views/rock_view.py
@@ -86,7 +86,10 @@ class RockOwnerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("first_name", "last_name")
+        fields = (
+            "first_name",
+            "last_name",
+        )
 
 
 class RockTypeSerializer(serializers.ModelSerializer):
